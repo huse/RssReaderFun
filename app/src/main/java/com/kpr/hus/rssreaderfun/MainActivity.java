b0b16043ddb5a2260803c881cb7e4617c39a9d8f
husen <arc78h@yahoo.com>
2015-10-27 23:11:08 -0700

    some code added

--- app/src/main/java/com/kpr/hus/rssreaderfun/MainActivity.java
+++ app/src/main/java/com/kpr/hus/rssreaderfun/MainActivity.java
@@ -2,12 +2,22 @@ package com.kpr.hus.rssreaderfun;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-    public class MainActivity extends Activity {
+public class MainActivity extends Activity {
         private PostData[] listData;
-
+      LayoutInflater inflater;
+    RelativeLayout headerRelativeLayout;
+    ImageView arrowImage;
+     ProgressBar progressBar;
+     TextView headerTextView;
+    TextView lastUpdateDateTextView;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -18,7 +28,37 @@ import android.widget.ListView;
             PostItemAdapter itemAdapter = new PostItemAdapter(this,
                     R.layout.postitem, listData);
             listView.setAdapter(itemAdapter);
+
         }
+
+    /*private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                long arg3) {
+            // TODO Auto-generated method stub
+            PostData data = listData.get(arg2 - 1);
+
+            Bundle postInfo = new Bundle();
+            postInfo.putString("content", data.postContent);
+
+            Intent postviewIntent = new Intent(MainActivity.this, PostViewActivity.class);
+            postviewIntent.putExtras(postInfo);
+            startActivity(postviewIntent);
+        }
+    };*/
+
+  /*  private void init(Context context) {
+            inflater = LayoutInflater.from(context);
+            headerRelativeLayout = (RelativeLayout)inflater.inflate(context, R.layout.refresh_header_view, null);
+            arrowImage = (ImageView)headerRelativeLayout.findViewById(R.id.head_arrowImageView);
+            progressBar = (ProgressBar)headerRelativeLayout.findViewById(R.id.head_progressBar);
+            headerTextView = (TextView)headerRelativeLayout.findViewById(R.id.head_tipsTextView);
+            lastUpdateDateTextView = (TextView)headerRelativeLayout.findViewById(R.id.head_lastUpdatedDateTextView);
+
+            headerRelativeLayout.setPadding(0, -1 * HEADER_HEIGHT, 0, 0);
+            this.addHeaderView(headerRelativeLayout, null, false);
+        } */
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/kpr/hus/rssreaderfun/PostViewActivity.java
@@ -0,0 +1,28 @@
+package com.kpr.hus.rssreaderfun;
+
+/**
+ * Created by f1 on 10/26/2015.
+ */
+
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.webkit.WebView;
+
+public class PostViewActivity extends Activity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.postview);
+        Bundle bundle = this.getIntent().getExtras();
+        String postContent = bundle.getString("content");
+
+        webView = (WebView)this.findViewById(R.id.webview);
+        webView.loadData(postContent, "text/html; charset=utf-8","utf-8");
+    }
+}
+
